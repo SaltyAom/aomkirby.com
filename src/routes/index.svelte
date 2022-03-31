1f7bba515267439ccd9081a524f3e13fa01cb9c2
saltyaom <saltyaom@gmail.com>
2022-03-31 17:18:55 +0700

    :tada: feat: init

---
+++ src/routes/index.svelte
@@ -0,0 +1,3 @@
+<main class="flex justify-center items-center w-full h-screen">
+    <h1 class="text-4xl">aomkirby</h1>
+</main>
---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import adapter from '@sveltejs/adapter-netlify';
+import preprocess from 'svelte-preprocess';
+
+import WindiCSS from 'vite-plugin-windicss'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter(),
+		vite: {
+			plugins: [WindiCSS()]
+		}
+	}
+};
+
+export default config;
